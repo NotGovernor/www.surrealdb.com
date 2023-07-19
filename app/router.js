d19a83b6b296560b366c7bded79d9e48fc30df94
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-07-19 15:29:07 +0100

    Full-text search documentation (#218)
    Co-authored-by: Micha de Vries <micha@devrie.sh>

--- app/router.js
+++ app/router.js
@@ -192,6 +192,7 @@ Router.map(function () {
 					this.route('event');
 					this.route('function');
 					this.route('field');
+					this.route('analyzer');
 					this.route('indexes');
 					this.route('param');
 				});
@@ -212,6 +213,7 @@ Router.map(function () {
 				this.route('meta');
 				this.route('parse');
 				this.route('rand');
+				this.route('search');
 				this.route('session');
 				this.route('string');
 				this.route('time');
